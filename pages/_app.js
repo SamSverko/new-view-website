19ae885bddb98883b39e166ae506ebf5a17550b7
Sam Sverko <sam.sverko@gmail.com>
2021-07-18 13:26:42 -0400

    Add code linting.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { GlobalStyle } from '../styles/global-style';
+import { GlobalStyle } from '../styles/global-style'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-        <GlobalStyle />
-        <Component {...pageProps} />
-    </>
-  )
-};
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    )
+}
