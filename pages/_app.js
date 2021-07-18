b4b724b97b2bb49a615749c79835db4f64e64e07
Sam Sverko <sam.sverko@gmail.com>
2021-07-18 13:12:33 -0400

    Add styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import { GlobalStyle } from '../styles/global-style';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+    </>
+  )
+};
